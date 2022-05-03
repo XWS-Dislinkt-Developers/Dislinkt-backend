119111ef79b34c1723e98f04cfb0f61724ca620c
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2022-05-03 23:45:53 +0200

    feat: user-post service (with problems)

---
+++ user_post_service/domain/user_post_store.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type UserPostStore interface {
+	Get(id primitive.ObjectID) (*UserPost, error)
+	GetAll() ([]*UserPost, error)
+	Insert(order *UserPost) error
+	DeleteAll()
+}
---
+++ user_post_service/go.mod
@@ -0,0 +1,3 @@
+module github.com/XWS-Dislinkt-Developers/Dislinkt-backend/user_post_servis
+
+go 1.18
