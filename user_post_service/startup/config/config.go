bb42acdc1e4bbbf98857f29f86680f0fb3a45aba
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-06-26 21:34:36 +0200

    bug: post_service docker try

--- user_post_service/startup/config/config.go
+++ user_post_service/startup/config/config.go
@@ -12,12 +12,16 @@ type Config struct {
 
 func NewConfig() *Config {
 	return &Config{
-		//Port:           "8002",
-		//UserPostDBHost: "localhost",
-		//UserPostDBPort: "27017",
-		//NatsHost:       "nats",
-		//NatsPort:       "4222",
-		//NatsUser:       "ruser",
-		//NatsPass:       "T0pS3cr3t",
+		Port:           "8002",
+		UserPostDBHost: "localhost",
+		UserPostDBPort: "27017",
+		NatsHost:       "nats",
+		NatsPort:       "4222",
+		NatsUser:       "ruser",
+		NatsPass:       "T0pS3cr3t",
+
+		//Port:           os.Getenv("USER_POST_SERVICE_PORT"),
+		//UserPostDBHost: os.Getenv("USER_POST_DB_HOST"),
+		//UserPostDBPort: os.Getenv("MONGO_DB_POST"),
 	}
 }
