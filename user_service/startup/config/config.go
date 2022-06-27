2f14774ff02db31ad8d6fc634ae63d62570e9751
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-06-27 13:00:22 +0200

    feat: local run

--- user_service/startup/config/config.go
+++ user_service/startup/config/config.go
@@ -1,7 +1,5 @@
 package config
 
-import "os"
-
 type Config struct {
 	Port       string
 	UserDBHost string
@@ -17,22 +15,22 @@ type Config struct {
 
 func NewConfig() *Config {
 	return &Config{
-		//Port:       "8003",
-		//UserDBHost: "localhost",
-		//UserDBPort: "5432",
-		//UserDBName: "users",
-		//UserDBUser: "postgres",
-		//UserDBPass: "postgres",
-		//NatsHost:   "nats",
-		//NatsPort:   "4222",
-		//NatsUser:   "ruser",
-		//NatsPass:   "T0pS3cr3t",
+		Port:       "8003",
+		UserDBHost: "localhost",
+		UserDBPort: "5432",
+		UserDBName: "users",
+		UserDBUser: "postgres",
+		UserDBPass: "postgres",
+		NatsHost:   "nats",
+		NatsPort:   "4222",
+		NatsUser:   "ruser",
+		NatsPass:   "T0pS3cr3t",
 
-		Port:       os.Getenv("USER_SERVICE_PORT"),
-		UserDBHost: os.Getenv("USER_DB_HOST"),
-		UserDBPort: os.Getenv("USER_DB_PORT"),
-		UserDBName: os.Getenv("USER_DB_NAME"),
-		UserDBUser: os.Getenv("USER_DB_USER"),
-		UserDBPass: os.Getenv("USER_DB_PASS"),
+		//Port:       os.Getenv("USER_SERVICE_PORT"),
+		//UserDBHost: os.Getenv("USER_DB_HOST"),
+		//UserDBPort: os.Getenv("USER_DB_PORT"),
+		//UserDBName: os.Getenv("USER_DB_NAME"),
+		//UserDBUser: os.Getenv("USER_DB_USER"),
+		//UserDBPass: os.Getenv("USER_DB_PASS"),
 	}
 }
