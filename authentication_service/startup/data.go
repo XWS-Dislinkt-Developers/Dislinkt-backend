e2990b885462807712ec732353325724be337d43
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-07-09 21:10:56 +0200

    feat: search done

--- authentication_service/startup/data.go
+++ authentication_service/startup/data.go
@@ -2,65 +2,71 @@ package startup
 
 import "github.com/XWS-Dislinkt-Developers/Dislinkt-backend/authentication_service/domain"
 
-//svima je lozinka pera
+//svima je lozinka Pera12345*
 var users = []*domain.User{
 
-	/*
-		{
-			ID:            1,
-			Username:      "Pera",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "pera@gmail.com",
-			IsItConfirmed: true,
-		},
-		{
-			ID:            2,
-			Username:      "Joka",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "joka@gmail.com",
-			IsItConfirmed: true,
-		},
-		{
-			ID:            3,
-			Username:      "Marko",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "marko@gmail.com",
-			IsItConfirmed: true,
-		},
-		{
-			ID:            4,
-			Username:      "Zeksa",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "zeksa@gmail.com",
-			IsItConfirmed: true,
-		},
-		{
-			ID:            5,
-			Username:      "Sanja",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "sanja@gmail.com",
-			IsItConfirmed: true,
-		},
-		{
-			ID:            6,
-			Username:      "Tanja",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "tanja@gmail.com",
-			IsItConfirmed: true,
-		}, {
-			ID:            7,
-			Username:      "Lale",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "lale@gmail.com",
-			IsItConfirmed: true,
-		}, {
-			ID:            8,
-			Username:      "Nena",
-			Password:      "$2a$10$.kVdlCEeYTeNEWURD.vXGeHYIIl0E/R.Wd0.Uly/IckXba1iwRFom",
-			Email:         "nena@gmail.com",
-			IsItConfirmed: true,
-		},
-
-
-	*/
+	{
+		ID:            1,
+		Username:      "Pera",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "pera@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            2,
+		Username:      "Joka",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "joka@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            3,
+		Username:      "Marko",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "marko@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            4,
+		Username:      "Zeksa",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "zeksa@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            5,
+		Username:      "Sanja",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "sanja@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            6,
+		Username:      "Tanja",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "tanja@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            7,
+		Username:      "Lale",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "lale@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
+	{
+		ID:            8,
+		Username:      "Nena",
+		Password:      "$2a$10$jVyQ6miOTsttmNHwGStv1OF1Z1sjGDP41LmWz33QZoVe4VyYx9NdS",
+		Email:         "nena@gmail.com",
+		IsItConfirmed: true,
+		Role:          "user",
+	},
 }
--- user_post_service/startup/data.go
+++ user_post_service/startup/data.go
@@ -3,153 +3,150 @@ package startup
 import (
 	"github.com/XWS-Dislinkt-Developers/Dislinkt-backend/user_post_service/domain"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 var userPosts = []*domain.UserPost{
-	/*
-		{
-			Id:        GetObjectId("507f1f77bcf86cd799439011"),
-			UserId:    1,
-			CreatedAt: time.Now(),
-			Text:      "First post!",
-			ImagePath: "imagepath..",
-			Reactions: []domain.Reaction{
-				{
-					UserId:   2,
-					Liked:    true,
-					Disliked: false,
-				},
-				{
-					UserId:   3,
-					Liked:    false,
-					Disliked: true,
-				},
-				{
-					UserId:   4,
-					Liked:    true,
-					Disliked: false,
-				},
+	{
+		Id:        GetObjectId("507f1f77bcf86cd799439011"),
+		UserId:    1,
+		CreatedAt: time.Now(),
+		Text:      "First post!",
+		ImagePath: "imagepath..",
+		Reactions: []domain.Reaction{
+			{
+				UserId:   2,
+				Liked:    true,
+				Disliked: false,
 			},
-			Comments: []domain.Comment{
-				{
-					UserId:    3,
-					CreatedAt: time.Now(),
-					Text:      "First comment!",
-				},
-				{
-					UserId:    3,
-					CreatedAt: time.Now(),
-					Text:      "Me again!",
-				},
-				{
-					UserId:    1,
-					CreatedAt: time.Now(),
-					Text:      "I can comment on my own post!!",
-				},
+			{
+				UserId:   3,
+				Liked:    false,
+				Disliked: true,
+			},
+			{
+				UserId:   4,
+				Liked:    true,
+				Disliked: false,
 			},
 		},
-		{
-			Id:        GetObjectId("507f1f77bcf86cd799439012"),
-			UserId:    2,
-			CreatedAt: time.Now(),
-			Text:      "Second post!",
-			ImagePath: "imagepath..",
-
-			Reactions: []domain.Reaction{
-				{
-					UserId:   1,
-					Liked:    true,
-					Disliked: false,
-				},
+		Comments: []domain.Comment{
+			{
+				UserId:    3,
+				CreatedAt: time.Now(),
+				Text:      "First comment!",
 			},
-			Comments: []domain.Comment{},
-		},
-		{
-			Id:        GetObjectId("6276eb70d31c8f2272d2fbe5"),
-			UserId:    4,
-			CreatedAt: time.Now(),
-			Text:      "New post!",
-			ImagePath: "imagepath..",
-
-			Reactions: []domain.Reaction{
-				{
-					UserId:   1,
-					Liked:    true,
-					Disliked: false,
-				},
-				{
-					UserId:   3,
-					Liked:    false,
-					Disliked: true,
-				},
-				{
-					UserId:   4,
-					Liked:    true,
-					Disliked: false,
-				},
+			{
+				UserId:    3,
+				CreatedAt: time.Now(),
+				Text:      "Me again!",
 			},
-			Comments: []domain.Comment{
-				{
-					UserId:    3,
-					CreatedAt: time.Now(),
-					Text:      "First comment!",
-				},
-				{
-					UserId:    3,
-					CreatedAt: time.Now(),
-					Text:      "Me again!",
-				},
-				{
-					UserId:    1,
-					CreatedAt: time.Now(),
-					Text:      "I can comment on my own post!!",
-				},
-			},
-		}, {
-			Id:        GetObjectId("6276eb70d31c8f2272d2fbe9"),
-			UserId:    1,
-			CreatedAt: time.Now(),
-			Text:      "Novi postic!",
-			ImagePath: "imagepath..",
-
-			Reactions: []domain.Reaction{
-				{
-					UserId:   5,
-					Liked:    true,
-					Disliked: false,
-				},
-				{
-					UserId:   3,
-					Liked:    false,
-					Disliked: true,
-				},
-				{
-					UserId:   4,
-					Liked:    true,
-					Disliked: false,
-				},
-			},
-			Comments: []domain.Comment{
-				{
-					UserId:    3,
-					CreatedAt: time.Now(),
-					Text:      "First comment!",
-				},
-				{
-					UserId:    3,
-					CreatedAt: time.Now(),
-					Text:      "Me again!",
-				},
-				{
-					UserId:    1,
-					CreatedAt: time.Now(),
-					Text:      "I can comment on my own post!!",
-				},
+			{
+				UserId:    1,
+				CreatedAt: time.Now(),
+				Text:      "I can comment on my own post!!",
 			},
 		},
+	},
+	{
+		Id:        GetObjectId("507f1f77bcf86cd799439012"),
+		UserId:    2,
+		CreatedAt: time.Now(),
+		Text:      "Second post!",
+		ImagePath: "imagepath..",
 
+		Reactions: []domain.Reaction{
+			{
+				UserId:   1,
+				Liked:    true,
+				Disliked: false,
+			},
+		},
+		Comments: []domain.Comment{},
+	},
+	{
+		Id:        GetObjectId("6276eb70d31c8f2272d2fbe5"),
+		UserId:    4,
+		CreatedAt: time.Now(),
+		Text:      "New post!",
+		ImagePath: "imagepath..",
 
-	*/
+		Reactions: []domain.Reaction{
+			{
+				UserId:   1,
+				Liked:    true,
+				Disliked: false,
+			},
+			{
+				UserId:   3,
+				Liked:    false,
+				Disliked: true,
+			},
+			{
+				UserId:   4,
+				Liked:    true,
+				Disliked: false,
+			},
+		},
+		Comments: []domain.Comment{
+			{
+				UserId:    3,
+				CreatedAt: time.Now(),
+				Text:      "First comment!",
+			},
+			{
+				UserId:    3,
+				CreatedAt: time.Now(),
+				Text:      "Me again!",
+			},
+			{
+				UserId:    1,
+				CreatedAt: time.Now(),
+				Text:      "I can comment on my own post!!",
+			},
+		},
+	}, {
+		Id:        GetObjectId("6276eb70d31c8f2272d2fbe9"),
+		UserId:    1,
+		CreatedAt: time.Now(),
+		Text:      "Novi postic!",
+		ImagePath: "imagepath..",
+
+		Reactions: []domain.Reaction{
+			{
+				UserId:   5,
+				Liked:    true,
+				Disliked: false,
+			},
+			{
+				UserId:   3,
+				Liked:    false,
+				Disliked: true,
+			},
+			{
+				UserId:   4,
+				Liked:    true,
+				Disliked: false,
+			},
+		},
+		Comments: []domain.Comment{
+			{
+				UserId:    3,
+				CreatedAt: time.Now(),
+				Text:      "First comment!",
+			},
+			{
+				UserId:    3,
+				CreatedAt: time.Now(),
+				Text:      "Me again!",
+			},
+			{
+				UserId:    1,
+				CreatedAt: time.Now(),
+				Text:      "I can comment on my own post!!",
+			},
+		},
+	},
 }
 
 func GetObjectId(id string) primitive.ObjectID {
